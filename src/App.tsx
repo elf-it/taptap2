4e53de376bace7b4e3cd445fcf4790e05f2d9927
elf-it <elf-it@yandex.ru>
2024-07-16 20:01:36 +0400

    add username

--- src/App.tsx
+++ src/App.tsx
@@ -136,7 +136,7 @@ function App() {
               <Mamont size={36} className='text-[#d4d4d4]' />
             </div>
             <div>
-              <p className='text-sm'>{tg.initData}</p>
+              <p className='text-sm'>{tg.initDataUnsafe?.user?.username}</p>
             </div>
           </div>
           <div className='flex items-center justify-between space-x-4 mt-1'>
