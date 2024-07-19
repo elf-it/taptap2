645bb58aa230073d40e1965ae8aaf39c2b7ced03
elf-it <elf-it@yandex.ru>
2024-07-19 21:29:21 +0400

    333

--- src/pages/ConnectWallet.jsx
+++ src/pages/ConnectWallet.jsx
@@ -1,11 +1,11 @@
-import { TonConnectButton, useTonConnectUI, useTonAddress } from "@tonconnect/ui-react";
+import { TonConnectButton, useTonConnectUI, useTonAddress, useTonWallet } from "@tonconnect/ui-react";
 
 
 export default function ConnectWallet() {
     const transaction = {
         messages: [
             {
-                address: "0:e4ced2de3d61c81e73a3214eb3725ce23405d172136d65940c7f52ce5a413871",
+                address: "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F",
                 amount: "20000000"
             }
         ]
@@ -26,6 +26,12 @@ export default function ConnectWallet() {
                         <span>User-friendly address: {userFriendlyAddress}</span>
                         <span>Raw address: {rawAddress}</span>
                     </div>
+                    wallet && (
+                        <div>
+                            <span>Connected wallet: {wallet.name}</span>
+                            <span>Device: {wallet.device.appName}</span>
+                        </div>
+                    )
                 )
             </div>
         </div>
