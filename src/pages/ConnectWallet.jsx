c0a67284fc20dedc260f67c6362cb3b00d94b4e4
elf-it <elf-it@yandex.ru>
2024-07-21 14:27:34 +0400

    send tx

--- src/pages/ConnectWallet.jsx
+++ src/pages/ConnectWallet.jsx
@@ -3,9 +3,10 @@ import { TonConnectButton, useTonConnectUI, useTonAddress, useTonWallet } from "
 
 export default function ConnectWallet() {
     const transaction = {
+        validUntil: Math.floor(Date.now() / 1000) * 360,
         messages: [
             {
-                address: "-1:e4ced2de3d61c81e73a3214eb3725ce23405d172136d65940c7f52ce5a413871",
+                address: "0:e4ced2de3d61c81e73a3214eb3725ce23405d172136d65940c7f52ce5a413871",
                 amount: "20000000"
             }
         ]
