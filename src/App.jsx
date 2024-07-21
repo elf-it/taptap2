999ff8f6bf3ae999d018d7d22376753a593b6948
elf-it <elf-it@yandex.ru>
2024-07-21 15:38:26 +0400

    set First Guid page

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,11 @@ import { binanceLogo } from './images';
 import AutoFarm from './pages/AutoFarm';
 import ConnectWallet from './pages/ConnectWallet';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
 
-  const [numPage, setNumPage] = useState(0);
+  const [numPage, setNumPage] = useState(1);
 
   const pages = [
     <Guide />,
@@ -24,29 +26,35 @@ function App() {
   ];
 
   return (
-    <TonConnectUIProvider manifestUrl='https://hammerhead-app-lqwus.ondigitalocean.app/tonconnect-manifest.json'>
+    <>
+    {tg.initDataUnsafe?.user?.username == "" ?
+      <TonConnectUIProvider manifestUrl='https://hammerhead-app-lqwus.ondigitalocean.app/tonconnect-manifest.json'>
 
-      <div className="bg-bgMain h-full bg-cover overflow-hidden">{pages[numPage]}</div>
+        <div className="bg-bgMain h-full bg-cover overflow-hidden">{pages[numPage]}</div>
 
-      <div className='fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs'>
-        <div className='text-center text-[#85827d] w-1/5 m-1 p-2 rounded-2xl' onClick={() => setNumPage(1)}>
-          <Mine className='w-8 h-8 mx-auto' />
-          <p className='mt-1'>Main</p>
+        <div className='fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs'>
+          <div className='text-center text-[#85827d] w-1/5 m-1 p-2 rounded-2xl' onClick={() => setNumPage(1)}>
+            <Mine className='w-8 h-8 mx-auto' />
+            <p className='mt-1'>Main</p>
+          </div>
+          <div className='text-center text-[#85827d] w-1/5 m-1 p-2 rounded-2xl' onClick={() => setNumPage(2)}>
+            <Friends className='w-8 h-8 mx-auto' />
+            <p className='mt-1'>Dasboard</p>
+          </div>
+          <div className='text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl' onClick={() => setNumPage(3)}>
+            <img src={binanceLogo} alt='Exchange' className='w-8 h-8 mx-auto' />
+            <p className='mt-1'>Autofarm</p>
+          </div>
+          <div className='text-center text-[#85827d] w-1/5 m-1 p-2 rounded-2xl' onClick={() => setNumPage(4)}>
+            <Coins className='w-8 h-8 mx-auto' />
+            <p className='mt-1'>Connect</p>
+          </div>
         </div>
-        <div className='text-center text-[#85827d] w-1/5 m-1 p-2 rounded-2xl' onClick={() => setNumPage(2)}>
-          <Friends className='w-8 h-8 mx-auto' />
-          <p className='mt-1'>Dasboard</p>
-        </div>
-        <div className='text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl' onClick={() => setNumPage(3)}>
-          <img src={binanceLogo} alt='Exchange' className='w-8 h-8 mx-auto' />
-          <p className='mt-1'>Autofarm</p>
-        </div>
-        <div className='text-center text-[#85827d] w-1/5 m-1 p-2 rounded-2xl' onClick={() => setNumPage(4)}>
-          <Coins className='w-8 h-8 mx-auto' />
-          <p className='mt-1'>Connect</p>
-        </div>
-      </div>
-    </TonConnectUIProvider>
+      </TonConnectUIProvider>
+      :
+      <div className="bg-bgMain h-full bg-cover overflow-hidden"><Guide /></div>
+    }
+    </>
   )
 }
 
