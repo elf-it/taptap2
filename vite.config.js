d7b96e37b5fa6ce52b99532feb62e05011153da2
elf-it <elf-it@yandex.ru>
2024-07-23 14:17:59 +0400

    buffer

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 1600 },
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ]
+    },
+    chunkSizeWarningLimit: 1600
+  },
   optimizeDeps: {
     esbuildOptions: {
       define: {
