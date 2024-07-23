031c32aafc6cab39f584f112826eeff7d7bafecb
elf-it <elf-it@yandex.ru>
2024-07-23 15:16:57 +0400

    test

--- src/component/BuyCardModal.jsx
+++ src/component/BuyCardModal.jsx
@@ -3,23 +3,16 @@ import moneyImgage from "../assets/images/money.png";
 import CustomSelect from "./CustomSelect";
 import Button from "./Button";
 import { useTonConnectUI } from "@tonconnect/ui-react";
-import { toNano, beginCell } from "@ton/ton";
 
 export default function BuyCardModal({ setShowModal, data }) {
   const [currentChoosedTarrif, setCurrentChoosedTarrif] = useState(0);
-  const [amount, setAmount] = useState(0);
-
-  const body = beginCell()
-  .storeUint(0, 32)
-  .storeStringTail(data.titleEn)
-  .endCell();
 
   const transaction = {
     validUntil: Math.floor(Date.now() / 1000) * 360,
     messages: [
       {
         address: "0:e4ced2de3d61c81e73a3214eb3725ce23405d172136d65940c7f52ce5a413871",
-        amount: toNano(data.tarrifs[currentChoosedTarrif]?.count),
+        amount: data.tarrifs[currentChoosedTarrif]?.count * 1000000000,
         payload: body.toBoc().toString("base64")
       }
     ]
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [
-        rollupNodePolyFill()
-      ]
-    },
-    chunkSizeWarningLimit: 1600
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  }
+  build: { chunkSizeWarningLimit: 1600 }
 })
