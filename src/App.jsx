5639eec3a4d289454d89a01c4d274a2344578680
elf-it <elf-it@yandex.ru>
2024-07-26 14:12:07 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import './App.css';
 import Guide from './pages/Guide';
 import Main from './pages/Main';
 import Dashboard from './pages/Dashboard';
+import Lottery from './pages/Lottery';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { useEffect, useState } from 'react';
-import Friends from './icons/Friends';
-import Mine from './icons/Mine';
-import Coins from './icons/Coins';
-import { binanceLogo } from './images';
 import AutoFarm from './pages/AutoFarm';
 import ConnectWallet from './pages/ConnectWallet';
-import Data from './pages/Data';
 import { getPerson } from './lib/fetch';
 import { Icon } from './component/IconSprite';
 
