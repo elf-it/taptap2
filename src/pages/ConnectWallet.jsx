d92900f9586ed8a51c649ad0f23fc2e8997c2dca
elf-it <elf-it@yandex.ru>
2024-07-26 22:42:52 +0400

    test

--- src/pages/ConnectWallet.jsx
+++ src/pages/ConnectWallet.jsx
@@ -4,12 +4,13 @@ import { useTonConnect } from "../hooks/useTonConnect";
 
 export default function ConnectWallet() {
 
-    const {network} = useTonConnect();
+    const {network, wallet} = useTonConnect();
 
     return(
         <>
-            <TonConnectButton className="w-full my-20px mt-50px" />
-            <p className="text-white">{network ? network === CHAIN.MAINNET ? "mainnet" : "testnet" : "N/A"}</p>
+            <TonConnectButton className="my-20px mt-50px" />
+            <p className="text-white">network: {network ? network === CHAIN.MAINNET ? "mainnet" : "testnet" : "N/A"}</p>
+            <p className="text-white">wallet: {wallet}</p>
         </>
     );
 }
