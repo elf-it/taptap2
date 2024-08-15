6bf28562ab8dd6a1e7d93c7446696ef9450b7445
elf-it <elf-it@yandex.ru>
2024-08-15 18:28:22 +0400

    test

--- src/helper/languages.js
+++ src/helper/languages.js
@@ -1,8 +1,8 @@
 export const allLanguages = {
     ru_lang: {fullName: "Русский", shortName: "RU"},
     en_lang: {fullName: "English", shortName: "EN"},
-    ch_lang: {fullName: "中文", shortName: "中文"},
-    cr_lang: {fullName: "Croatian", shortName: ""},
+    pt_lang: {fullName: "Português", shortName: "PT"},
+    es_lang: {fullName: "Español", shortName: "ES"},
     cz_lang: {fullName: "Czech", shortName: ""},
     da_lang: {fullName: "Danish", shortName: ""}
 }
@@ -10,11 +10,11 @@ export const allLanguages = {
 export const suggestedLanguages = {
     ru_lang: allLanguages.ru_lang,
     en_lang: allLanguages.en_lang,
+    pt_lang: allLanguages.pt_lang,
+    es_lang: allLanguages.es_lang,
 };
 
 export const otherLanguages = {
-    ch_lang: allLanguages.ch_lang,
-    cr_lang: allLanguages.cr_lang,
     cz_lang: allLanguages.cz_lang,
     da_lang: allLanguages.da_lang
 };
